5ddf20e693f74273fafb9966713c5571288ead16
Daniil <danber005@yandex.ru>
2024-11-20 20:07:16 +0500

    Convert array to stringstream

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <fstream>
 #include <vector>
 #include <algorithm>
+#include <sstream>
 using namespace std;
 vector<vector<string>> arr; //Создаем двумерный массив строк
 ifstream in("test.txt"); // Читаем файл
@@ -17,7 +18,10 @@ void printLines() {
 
 }
 void writeFile() {
-
+    stringstream sstream;
+    for (int i = 0; i < arr.size(); i++) {
+        sstream << arr[i][0] << " " << arr[i][1] << endl;
+    }
 }
 
 int main()
