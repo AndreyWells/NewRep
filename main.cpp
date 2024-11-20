06802a801387e8bf4d9defa32e5ec8515c2d10f9
Andrey <vikakaef@inbox.ru>
2024-11-20 18:54:16 +0500

    Completed the template

--- main.cpp
+++ main.cpp
@@ -1,4 +1,8 @@
 #include <iostream>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 void readFile() {
@@ -16,6 +20,9 @@ void writeFile() {
 
 int main()
 {
+    SetConsoleOutputCP(CP_UTF8);
+    vector<vector<string>> arr; //Создаем двумерный массив строк
+    ifstream in("test.txt"); // Читаем файл
     readFile();
     sortLines();
     printLines();
